1e347f09217f42aedde4e8fbcd73dd7e052740c0
MmdRza.ebrahimi <moh.ebrahimi@bluteam.ir>
2025-11-09 09:09:15 +0330

    feat: Initial project setup with Spring Boot and Docker Compose for Oracle

---
+++ src/test/java/com/mrebrahimi/academicpaperworkflow/AcademicPaperWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrebrahimi.academicpaperworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicPaperWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
