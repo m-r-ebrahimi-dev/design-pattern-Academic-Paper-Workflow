73fd4f7a96995187c0c57d580c236fcd0e6a5c0f
MmdRza.ebrahimi <moh.ebrahimi@bluteam.ir>
2025-11-09 09:02:55 +0330

    feat: Initial project setup with Spring Boot and Docker Compose for Oracle

---
+++ src/main/java/com/mrebrahimi/academicpaperworkflow/AcademicPaperWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.mrebrahimi.academicpaperworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicPaperWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicPaperWorkflowApplication.class, args);
+    }
+
+}
